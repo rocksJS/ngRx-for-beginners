90751cb811c37f4bdd64280bb10229c1f42c07a6
rocksJS <6abyghost@gmail.com>
2022-10-05 11:44:25 +0300

    ngrx-1 add counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ngrx-for-beginners';
+  counter: number = 0;
+  updatedAt?: number;
+
+  get cannotDecrease(): boolean {
+    return this.counter <= 0;
+  }
+
+  increase(): void {
+    this.updatedAt = Date.now();
+    this.counter++;
+  }
+
+  decrease(): void {
+    this.updatedAt = Date.now();
+    this.counter--;
+  }
+
+  counterClear(): void {
+    this.updatedAt = Date.now();
+    this.counter = 1;
+  }
 }
